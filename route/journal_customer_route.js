d1233a057ae3371e13449cb5b539e7e64d13cd3b
DhrumitBoricha9054 <dhrumitboricha8@gmail.com>
2025-12-16 15:49:10 +0530

    bug solve

--- route/journal_customer_route.js
+++ route/journal_customer_route.js
@@ -24,30 +24,4 @@ router.post('/delete', checkPermission(permission), journalCustomerController.de
 
 module.exports = router;
 
-const express = require('express');
-const router = express.Router();
-const journalCustomerController = require('../controller/journal_customer_controller');
-const authMiddleware = require('../middlewares/auth');
-const checkPermission = require('../middlewares/checkPermission');
-
-// All routes require authentication
-router.use(authMiddleware);
-
-// Use same permission as journal entries
-const permission = 'journal_entries';
-
-// Get all customers (for dropdown and table)
-router.post('/getAll', checkPermission(permission), journalCustomerController.getAll);
-
-// Create new customer
-router.post('/', checkPermission(permission), journalCustomerController.create);
-
-// Update customer
-router.post('/update', checkPermission(permission), journalCustomerController.update);
-
-// Delete customer
-router.post('/delete', checkPermission(permission), journalCustomerController.delete);
-
-module.exports = router;
-
 
